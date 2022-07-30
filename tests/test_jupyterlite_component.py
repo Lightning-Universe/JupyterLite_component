01cfe25caaceeb497703d110b137457fdee4b1c5
Aniket Maurya <theaniketmaurya@gmail.com>
2022-07-30 14:05:09 +0530

    fix tests

--- tests/test_jupyterlite_component.py
+++ tests/test_jupyterlite_component.py
@@ -9,7 +9,7 @@ from lai_jupyterlite.component import JupyterLite
 
 def test_run(tmp_path):
     port = int("80" + str(random.randrange(10, 99)))
-    jupyter = JupyterLite(port=port, contents=str(tmp_path))
+    jupyter = JupyterLite(port=port, host="0.0.0.0", contents=str(tmp_path))
     thread = Thread(target=jupyter.run, daemon=True)
     thread.start()
     sleep(10)
